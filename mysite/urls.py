6dd7ab7c2aa7618779c816e6bdf9c926a207cd61
mdmkl6 <mdmkl1gim@gmail.com>
2020-12-06 17:45:03 +0000

    sing in and login full

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls import url
 from mysite import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls), 
-    url('signup/', views.signup),  
-    url('/',views.home), 
+    url('signup/', views.signup, name='signup'),  
+    # url('home/',views.home),
+    path('', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'), 
 ]
