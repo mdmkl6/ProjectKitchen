207a0bd80c6c46e78b4d0e14e21429e6e6bdd58a
repl.it user <replituser@example.com>
2020-12-07 09:43:38 +0000

    More duplicate and nonsence products removed

--- products/fixtures/initialize_products.py
+++ products/fixtures/initialize_products.py
@@ -12,11 +12,17 @@ def product_list_from_csv(csv_name):
   return list(products)
 
 def clean_product_names(products):
+  #usuwanie pustego stringu i elementów ingredientXY od 01 do 06
   products.remove("")
+  for i in range(1,6):
+    products.remove("Ingredient0" + str(i))
   #usuwanie wszystkiego po przecinku
   new_list = list(map(lambda product: product.split(",", 1)[0], products))
   #usuwanie wszystkiego w nawiasie
   new_list = list(map(lambda product: product.split(" (", 1)[0], new_list))
+  #usuwanie powtarzajacych sie elementow
+  new_list = list(set(new_list))
+  print(len(new_list))
   return new_list
 
 products = product_list_from_csv("recipes.csv")
@@ -27,6 +33,8 @@ json_data = [{"model": "products.product", "pk": i, "fields": {"name": name}} fo
 with open("initial_products.json",'w') as f: 
   json.dump(json_data, f, indent=1) 
 
+#Teraz żeby zainicjalizować produkty wystarczy wpisać komendę:
+#python manage.py loaddata initial_products.json
 
 
 
