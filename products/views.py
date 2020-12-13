b77fcd8afb9a51a0a25c1ebabb9f440303dbaff8
repl.it user <replituser@example.com>
2020-12-13 15:29:49 +0000

    changes to merge with testowy

--- products/views.py
+++ products/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Product
 # Create your views here.
+
+def products_view(request):
+  all_products = Product.objects.all()
+  return render(request, 'products.html', {"all_items": all_products})
